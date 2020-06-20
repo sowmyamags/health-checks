d44d719cb0262d82886de89b7d791517b97e721c
sowmaymags <sowmyamaguluri@gmail.com>
2020-06-20 17:15:30 -0400

    in disk_usage.py rename min_absolute to min_gb use parameters names for indication"
    q

--- disk_usage.py
+++ disk_usage.py
@@ -2,22 +2,32 @@
 
 import shutil
 import sys
+import os 
 
-def check_disk_usage(disk, min_absolute, min_percent):
+def check_reboot():
+   """Returns True id the computer has a pending reboot."""
+   return os.path.exists("/run/reboot.required")
+
+def check_disk_full(disk, min_gb, min_percent):
    """Rerturns True if there is enough free disk space, false otherwise"""
    du = shutil.disk_usage(disk)
-   #Calculate the percentage of freespace
+   #Calculate the percentage of free space
    percent_free = 100 * du.free / du.total
    # Calculate how many gigabytes
    gigabytes_free = du.free / 2**30
-   if percent_free < min_percent or gigabytes_free < min_absolute:
+   if percent_free < min_percent or gigabytes_free < min_gb:
       return False
    return True
 
-#check for a least 2 GB and 10% free
-if not check_disk_usage("/", 1, 10):
-   print("ERROR: N0t enough disk usage")
-   sys.exit(1)
+def main():
+   if check_reboot():
+      print("Pending Reboot.")
+      sys.exit(1)
+   if check_disk_full("/", min_gb = 2, min_percent = 10):
+      print("Disk Full.")
+      sys.exit(1)
 
-print("Everything Ok")
-sys.exit(0)
+   print("Everything ok.")
+   sys.exit(0)
+
+main()
